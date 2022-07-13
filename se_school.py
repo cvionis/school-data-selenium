d6c28355bd5ea4e847be5a554cea5edc9b2281e7
Chance Vionis <cvionis03@gmail.com>
2022-07-13 15:08:10 -0400

    Set up chromedriver Save relative path of chromedriver to variable Set up driver using driverPath Get url of school website

--- se_school.py
+++ se_school.py
@@ -2,3 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import os 
 import time
+
+driverPath = os.path.abspath('chromedriver')
+driver = webdriver.Chrome(driverPath)
+driver.get('https://academy.abeka.com/Video2/Streaming/Default.aspx?cs=y')
+
