a4a9d81728dc97a984ca79985895fbcc9548982c
Chance Vionis <cvionis03@gmail.com>
2022-07-22 20:49:16 -0400

    Refactor code
    Remove 'camel case' naming for variables and replace with 'snake case'

--- se_school.py
+++ se_school.py
@@ -5,32 +5,32 @@ import time
 import datetime
 
 def main():
-    driverPath = os.path.abspath('chromedriver')
-    driver = webdriver.Chrome(driverPath)
+    driver_path = os.path.abspath('chromedriver')
+    driver = webdriver.Chrome(driver_path)
     driver.get('https://academy.abeka.com/Video2/Streaming/Default.aspx?cs=y')
 
     driver.implicitly_wait(20)
 
-    lessonsGroup = driver.find_element(By.ID, 'dvLessonsToday')
+    lessons_group = driver.find_element(By.ID, 'dvLessonsToday')
 
-    subjList = []
-    subjects = lessonsGroup.find_elements(By.CLASS_NAME, 'dailyLesson')
+    subj_list = []
+    subjects = lessons_group.find_elements(By.CLASS_NAME, 'dailyLesson')
 
     for s in subjects:
-        subjList.append(s.text.replace('\n',':'))
+        subj_list.append(s.text.replace('\n',':'))
 
-    subjList = list(filter(None, subjList))
-    subjDict = {}
+    subj_list = list(filter(None, subj_list))
+    subj_dict = {}
     
     print('\nSubjects:\n')
 
-    for subj in subjList:
-        subjName = subj.rsplit(':',1)[0]
-        lessNumStr = subj.rsplit(':', 1)[-1] # get string ('Lesson 00') following colon in each subject in subjList
-        lessNum = int(lessNumStr.rsplit(' ', 1)[-1]) # get number of lessNumStr
-        subjDict[subjName] = lessNum # create new dict entry for each subject with subjName as key and lessNum as value
+    for subj in subj_list:
+        subj_name = subj.rsplit(':',1)[0]
+        less_num_str = subj.rsplit(':', 1)[-1] # get string ('Lesson 00') following colon in each subject in subj_list
+        less_num = int(less_num_str.rsplit(' ', 1)[-1]) # get number of less_num_str
+        subj_dict[subj_name] = less_num # create new dict entry for each subject with subj_name as key and less_num as value
         
-        print(f'{subjName} | {lessNumStr}')
+        print(f'{subj_name} | {less_num_str}')
     
     current_date = datetime.datetime.now()
     current_date_str = current_date.strftime(f'%m/%d/%y')
@@ -49,9 +49,9 @@ def main():
     except:
         print('Enter a valid input (1,2, etc.)\n')
     
-    lesson_num_sum = sum([subjDict[s] for s in subjDict if subjDict[s] >= 50]) # don't include subject lesson numbers < 50 in lesson number sum (e.g. lesson numbers for classes that haven't been started)
+    lesson_num_sum = sum([subj_dict[s] for s in subj_dict if subj_dict[s] >= 50]) # don't include subject lesson numbers < 50 in lesson number sum (e.g. lesson numbers for classes that haven't been started)
     
-    lesson_num_avg = round((lesson_num_sum/(len(subjDict)-1)), 0)
+    lesson_num_avg = round((lesson_num_sum/(len(subj_dict)-1)), 0)
     final_lesson_num = 170
     lessons_left = final_lesson_num - lesson_num_avg
     
